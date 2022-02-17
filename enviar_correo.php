7389c64ce80124105648ee9956eb7ec01699f8b3
Mguelagustini <miguelagustini2015@gmail.com>
2022-02-17 16:04:21 -0300

    aa

--- enviar_correo.php
+++ enviar_correo.php
@@ -1,5 +1,5 @@
 <?php 
-$nombre = $_POST ['nombre'];
+/*$nombre = $_POST ['nombre'];
 $email = $_POST ['email'];
 $telefono = $_POST ['telefono'];
 $consulta = $_POST ['consulta'];
@@ -13,5 +13,16 @@ $header = "From: dekanor@gmail.com" . "\r\n" . "CCsomebodyelse@example.com";
 
 mail ($para, $mensaje_nombre, $mensaje_telefono, $header);
 
-header('Location: contacto.html');
+header('Location: contacto.html');*/
+
+
+
+$to = "miguelagustini2015@gmail.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+mail($to,$subject,$txt,$headers);
+
 ?>
