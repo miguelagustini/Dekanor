b28520ef2fc1394492d8d0b0179851c7762b7ca6
Mguelagustini <miguelagustini2015@gmail.com>
2022-02-21 15:01:38 -0300

    aaaa

--- enviar_correo.php
+++ enviar_correo.php
@@ -1,5 +1,5 @@
 <?php 
-/*$nombre = $_POST ['nombre'];
+$nombre = $_POST ['nombre'];
 $email = $_POST ['email'];
 $telefono = $_POST ['telefono'];
 $consulta = $_POST ['consulta'];
@@ -7,22 +7,30 @@ $consulta = $_POST ['consulta'];
 
 $para = "miguelagustini2015@gmail.com";
 $mensaje_nombre = "Nombre: " . $nombre;
-$mensaje_telefono = "Telefono: " . $telefono . " Consulta: " . $nombre;
+$mensaje_telefono = "Telefono: " . $telefono . " Consulta: " . $consulta;
 $header = "From: dekanor@gmail.com" . "\r\n" . "CCsomebodyelse@example.com";
+$header .= "Content-type: text/html\r\n";
 
 
 mail ($para, $mensaje_nombre, $mensaje_telefono, $header);
 
-header('Location: contacto.html');*/
+echo ($para .' ' . $mensaje_nombre . ' ' . $mensaje_telefono);
 
 
+
+/*
 
 $to = "miguelagustini2015@gmail.com";
 $subject = "My subject";
 $txt = "Hello world!";
 $headers = "From: webmaster@example.com" . "\r\n" .
 "CC: somebodyelse@example.com";
+$header .= "Content-type: text/html\r\n";
 
 mail($to,$subject,$txt,$headers);
+
+header('Location: contacto.html');
+
+?>*/
 
 ?>
